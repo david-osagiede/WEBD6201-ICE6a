1ba05c3689a685cc523e7c2b8217944f616fe370
david osagiede <yungospaghetti@gmail.com>
2021-03-07 23:38:25 -0500

    initial commit

--- WEBD6201-W2021-lab2-main/Scripts/app.js
+++ WEBD6201-W2021-lab2-main/Scripts/app.js
@@ -1,5 +1,5 @@
 /* custom JavaScript goes here */
-
+//name: david osagiede date:3/7/2021 ID:100739445
 //IIFE - Immediately Invoked Function Expression
 //AKA - Anonymous Self-Executing Function
 //Closure - limits scope leak
@@ -8,6 +8,138 @@
 
 ((core) =>
 {
+  //Create a JavaScript Class named User, in the same file (app.js) but above the IIFE
+  class User 
+  {
+    // getters and setters
+    get DisplayName() 
+    {
+      return this.m_displayName;
+    }
+  
+    set DisplayName(value) 
+    {
+      this.m_displayName = value;
+    }
+  
+    get EmailAddress() 
+    {
+      return this.m_emailAddress;
+    }
+  
+    set EmailAddress(value) 
+    {
+      this.m_emailAddress = value;
+    }
+
+    get Username() 
+    {
+      return this.m_username;
+    }
+  
+    set Username(value) 
+    {
+      this.m_username = value;
+    }
+
+    get Password() 
+    {
+      return this.m_password;
+    }
+  
+    set Password(value) 
+    {
+      this.m_password = value;
+    }
+  
+    // constructor
+
+    /**
+     * Creates an instance of User.
+     * @param {string} [displayName=""]
+     * @param {string} [emailAddress=""]
+     * @param {string} [username=""]
+     * @param {string} [password=""]
+     */
+    constructor(displayName = "", emailAddress = "", username = "", password="") 
+    {
+      this.DisplayName = displayName;
+      this.EmailAddress = emailAddress;
+      this.Username = username;
+      this.Password = password;
+    }
+
+    // methods
+
+    /**
+     * This method overrides the built-in toString method for the User class
+     *
+     * @returns {string}
+     */
+    toString() 
+    {
+      return `Display Name     : ${this.DisplayName} \nEmail Address : ${this.EmailAddress} \nUsername : ${this.Username}`;
+    }
+
+    /**
+     * This method returns a JSON object made up of the properties of the User class
+     *
+     * @returns {Object}
+     */
+    toJSON()
+    {
+      return {
+        "DisplayName": this.DisplayName,
+        "EmailAddress": this.EmailAddress,
+        "Username": this.Username
+      }
+    }
+
+    /**
+     * This method takes a JSON data object and assigns the values to the User class properties
+     *
+     * @param {Object} data
+     */
+    fromJSON(data)
+    {
+      this.DisplayName = data.DisplayName;
+      this.EmailAddress = data.EmailAddress;
+      this.Username = data.Username;
+      this.Password = data.Password;
+    }
+
+    /**
+     * This method converts the User into a comma-separated value string
+     *
+     * @returns {string}
+     */
+    serialize()
+    {
+      if(this.DisplayName !== "" && this.EmailAddress !== "" && this.Username !== "")
+      {
+        return `${this.DisplayName},${this.EmailAddress},${this.Username}`;
+      }
+      else 
+      {
+        console.error("One or more properties of the User is empty");
+        return null;
+      }
+    }
+
+    /**
+     * This method takes a comma-separated data string and assigns the values to the User class properties
+     *
+     * @param {string} data
+     * @return {void}
+     */
+    deserialize(data)
+    {
+      let propertyArray = data.split(",");
+      this.DisplayName = propertyArray[0];
+      this.EmailAddress = propertyArray[1];
+      this.Username = propertyArray[2];
+    }
+  }
   /**
    * This function loads the header and marks the related nav link active
    *
@@ -413,7 +545,7 @@
        toggleLogin();
         
     }
-
+    
     window.addEventListener("load", Start);
 
     core.Start = Start;
